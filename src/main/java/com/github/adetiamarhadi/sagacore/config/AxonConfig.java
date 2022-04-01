49af618a76679e4cb1f76ac1ff5f09f30b0025cb
Adet <adetia.marhadi>
2022-04-01 11:01:20 +0700

    base model for services

---
+++ src/main/java/com/github/adetiamarhadi/sagacore/config/AxonConfig.java
@@ -0,0 +1,21 @@
+package com.github.adetiamarhadi.sagacore.config;
+
+import com.thoughtworks.xstream.XStream;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AxonConfig {
+
+	@Bean
+	public XStream xStream() {
+
+		XStream xStream = new XStream();
+
+		xStream.allowTypesByWildcard(new String[] {
+				"com.github.adetiamarhadi.**"
+		});
+
+		return xStream;
+	}
+}
