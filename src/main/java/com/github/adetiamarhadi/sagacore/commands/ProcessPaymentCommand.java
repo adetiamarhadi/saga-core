5a2711d3881e03de64a41fd0a00aec322213cb99
adetiamarhadi <adetia.marhadi@gmail.com>
2022-04-03 22:38:03 +0700

    add @TargetAggregateIdentifier on paymentId

--- src/main/java/com/github/adetiamarhadi/sagacore/commands/ProcessPaymentCommand.java
+++ src/main/java/com/github/adetiamarhadi/sagacore/commands/ProcessPaymentCommand.java
@@ -3,11 +3,13 @@ package com.github.adetiamarhadi.sagacore.commands;
 import com.github.adetiamarhadi.sagacore.model.PaymentDetails;
 import lombok.Builder;
 import lombok.Data;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
 
 @Data
 @Builder
 public class ProcessPaymentCommand {
 
+    @TargetAggregateIdentifier
     private final String paymentId;
     private final String orderId;
     private final PaymentDetails paymentDetails;
